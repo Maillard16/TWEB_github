15f31c7c7ffdffc051a86950d70a84b4dd7fde87
Maillard16 <stephane.maillard@heig-vd.ch>
2014-10-06 16:21:19 +0200

    remove old directory

--- TWEB-express/routes/json-query.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res) {
-    
-    res.send("json query <br/> get => " + req.query.s);
-});
-
-module.exports = router;
